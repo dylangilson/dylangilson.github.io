830407e8f8bf4a1cff7ea4ba4500255197052b7a
dylangilson <dylan.gilson@outlook.com>
2023-01-11 14:04:53 -0800

    added emailjs

--- src/Pages/EmailForm.js
+++ src/Pages/EmailForm.js
@@ -4,11 +4,41 @@
  * February 10, 2022
  */
 
-import React from 'react';
+// TODO: style the form
+
+import React, { useRef } from 'react';
+import emailjs from "emailjs-com"
+import { EmailFormWrapper } from './EmailFormComponents';
 
 const EmailForm = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm("service_4nksptj", "template_zo5y5ze", form.current, "ZovflCav9CsmWp8AV").then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+        e.target.reset()
+    }
+
     return (
-        <h1>Email Page</h1>
+        <EmailFormWrapper>
+            <form ref={form} onSubmit={sendEmail}>
+                <input type="text" className="form-control" placeholder="Name" name="name" style={{ justifyContent: "center", alignItems: "center" }} />
+                <br />
+                <input type="email" className="form-control" placeholder="Email Address" name="email" />
+                <br />
+                <input type="text" className="form-control" placeholder="Subject" name="subject" />
+                <br />
+                <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message" />
+                <br />
+                <input type="submit" className="btn" value="Send Email" />
+            </form>
+        </EmailFormWrapper>
     );
 }
 
---
+++ src/Pages/EmailFormComponents.js
@@ -0,0 +1,18 @@
+/*
+ * Dylan Gilson
+ * dylan.gilson@outlook.com
+ * January 11, 2023
+ */
+
+import styled from 'styled-components';
+
+export const EmailFormWrapper = styled.div`
+  padding: 48px 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 16px;
+  max-width: 1100px;
+  margin: 0 auto;
+`;
