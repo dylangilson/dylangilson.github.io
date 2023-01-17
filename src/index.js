f68bd6e00c6003af99b145ede8e58569ab501afa
dylangilson <dylan.gilson@outlook.com>
2023-01-17 00:53:52 -0800

    removed CSS from pages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
  * January 31, 2022
  */
 
-// NOTE: launch webapp with command: yarn start
+// NOTE: launch webapp locally with command: yarn start
 // NOTE: update the git by running the following commands: git add . ; git commit -m "message" ; git push origin master ; npm run deploy
 // TODO: update README.md
 // TODO: remove background video and add a custom CSS design
